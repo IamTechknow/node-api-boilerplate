da2579373a31485445d8d3e6956548658f005867
Edmond Lee <techhexium@gmail.com>
2021-03-14 12:19:31 -0700

    Revise initialization code, add helper methods to handle response

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const { getDB } = require('../models');
+
+let db;
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -7,16 +11,38 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+  _sendOkJSON({ express: 'Hello From Express' }, res);
 });
 
 app.post('/api/world', (req, res) => {
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  _sendOkJSON(
+    { express: `I received your POST request. This is what you sent me: ${req.body.post}` },
+    res,
+    201
   );
 });
 
-const server = app.listen(port, () => console.log(`Listening on port ${port}`));
-process.on("SIGINT", () => {
-  server.close();
-});
+const _sendOkJSON = (obj, res, code = 200) => {
+  res.status(code).type('application/json').send(JSON.stringify(obj));
+};
+
+const _sendErrorJSON = (message, res, code = 400) => {
+  res.status(code).type('application/json').send(JSON.stringify({ message }));
+};
+
+const _catchError = (err, res) => {
+  console.error(err);
+  _sendErrorJSON('Sorry, an error occurred!', res, 500);
+};
+
+const runServer = async () => {
+  db = await getDB();
+
+  const server = app.listen(port, () => console.log(`Listening on port ${port}`));
+  process.on("SIGINT", () => {
+    db.end();
+    server.close();
+  });
+};
+
+runServer();
